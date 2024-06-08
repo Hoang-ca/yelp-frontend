57a0ceda7f30b891a85632cef265a0641d788b93
Hoang <vmhdaica123@gmail.com>
2024-06-08 16:36:16 +0700

    Normalize all line endings to CRLF

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
 import { LandingPage } from "./LandingPage/LandingPage";
+import { Search } from "./Search/Search";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <LandingPage/>
+    <Routes>
+      <Route path='/search' element={<Search />} />
+      <Route path='/' element={<LandingPage />} />
+    </Routes>
+
   );
 }
 
