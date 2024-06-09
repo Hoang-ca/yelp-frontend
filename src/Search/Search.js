4dac645da4f5763cae4fb541cde0d90126020fbd
Hoang <vmhdaica123@gmail.com>
2024-06-09 16:55:54 +0700

    make app more dinamic

--- src/Search/Search.js
+++ src/Search/Search.js
@@ -3,14 +3,20 @@ import { NavBar } from "../NavBar/NavBar";
 import { SubNav } from "../NavBar/SubNav/SubNav";
 import { SearchResultsSummary } from "./SearchResultsSummary/SearchResultsSummary";
 import { SearchResults } from "./SearchResults/SearchResults";
+import { useLocation } from 'react-router-dom';
 
-export function Search(){
-    return(
+export function Search() {
+    const location = useLocation();
+    const params = new URLSearchParams(location.search);
+    const term = params.get('find_desc');
+    const locationParam = params.get('find_loc');
+
+    return (
         <div>
-            <NavBar />
+            <NavBar term={term} location={locationParam} />
             <SubNav />
-            <SearchResultsSummary />
-            <SearchResults/>
+            <SearchResultsSummary term={term} location={locationParam} />
+            <SearchResults />
         </div>
     );
 }
